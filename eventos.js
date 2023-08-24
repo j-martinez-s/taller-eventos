cc4ac150966538eb09ce7cee0c734269def6fce8
Jimena <jimemartinez0506@gmail.com>
2023-08-24 03:05:31 -0300

    alert del div

--- eventos.js
+++ eventos.js
@@ -1,7 +1,18 @@
+function alerta(){
+    alert("¡Hola!");
+}
+
 function cartelito (){
     alert("¡Hola! ¡Soy el div!");
 }
 
-var miDiv = document.getElementsById("unDiv");
+document.addEventListener("DOMContentLoaded", function(){
+const miDiv = document.getElementById("div");
+const boton = document.getElementById("boton");
 
-miDiv.addEventListener("click", cartelito);
+miDiv.addEventListener("click", function(ev){
+    if (ev.target != boton){
+        cartelito()
+    }
+});
+})
